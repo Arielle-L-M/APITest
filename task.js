4006aa4f69405c714b56ea00cd6e7ac8c54ea4af
Arielle Moon <amoon@icstars.org>
2026-03-09 15:45:44 -0500

    Task complete TuT

--- task.js
+++ task.js
@@ -4,11 +4,17 @@ require('dotenv').config();
 
 const apiFetch = async () => {
   try {
-    const response = await fetch(`    `);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
     const data = await response.json();
     console.log("First Post Title:", data[0]?.title);
-  } 
+
+
+  } catch (error) {
+      console.log("Error Attempting To Fetch Post Titles", error.message)
+    }
 };
+
+apiFetch();
 
